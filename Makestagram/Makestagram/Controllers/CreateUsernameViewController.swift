acd036f8c0ce865a8f229405ba3582ebe6a63118
Yash Bengali <yashbfalcon@gmail.com>
2018-07-12 08:58:01 -0700

    Firebase login + basic ui
    Basic UI of the app along with firebase sign in

---
+++ Makestagram/Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Yash on 7/11/18.
+//  Copyright © 2018 Yash. All rights reserved.
+//
+
+import UIKit
+
+class CreateUsernameViewController: UIViewController {
+    
+}
+
---
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Yash on 7/11/18.
+//  Copyright © 2018 Yash. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController{
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else {return}
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+
+}
+
+extension LoginViewController: FUIAuthDelegate{
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error{
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        guard let user = user
+            else {return}
+        
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        userRef.observeSingleEvent(of: .value, with: {(snapshot) in
+            if let user = User(snapshot: snapshot){
+                print ("User already exists \(user.username)")
+            }
+            else{
+                print("New user! :)")
+            }
+        })
+    }
+}
